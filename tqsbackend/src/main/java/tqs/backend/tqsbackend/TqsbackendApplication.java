2e2797f627c15e95816401c86a460f564305b149
Goncsal <goncalodfalmeida@ua.pt>
2025-11-11 12:39:13 +0000

    pushing existing springboot project

---
+++ tqsbackend/src/main/java/tqs/backend/tqsbackend/TqsbackendApplication.java
@@ -0,0 +1,13 @@
+package tqs.backend.tqsbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TqsbackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TqsbackendApplication.class, args);
+	}
+
+}
---
+++ tqsbackend/src/test/java/tqs/backend/tqsbackend/TqsbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package tqs.backend.tqsbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TqsbackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
