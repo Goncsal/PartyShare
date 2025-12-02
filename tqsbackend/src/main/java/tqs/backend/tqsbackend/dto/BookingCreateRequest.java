8e36d3bb14ead7180f9fcf2d2702864cd7ef7d1b
João Barreira <joaobarreira05@ua.pt>
2025-12-02 08:32:06 +0000

    PS-38 test:  coverage

--- tqsbackend/src/main/java/tqs/backend/tqsbackend/dto/BookingCreateRequest.java
+++ tqsbackend/src/main/java/tqs/backend/tqsbackend/dto/BookingCreateRequest.java
@@ -4,7 +4,13 @@ import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.FutureOrPresent;
 import jakarta.validation.constraints.NotNull;
 import java.time.LocalDate;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
 public class BookingCreateRequest {
 
     @NotNull
@@ -20,36 +26,4 @@ public class BookingCreateRequest {
     @NotNull
     @Future
     private LocalDate endDate;
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
-
-    public Long getRenterId() {
-        return renterId;
-    }
-
-    public void setRenterId(Long renterId) {
-        this.renterId = renterId;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
 }
--- tqsbackend/src/main/java/tqs/backend/tqsbackend/dto/BookingResponse.java
+++ tqsbackend/src/main/java/tqs/backend/tqsbackend/dto/BookingResponse.java
@@ -2,9 +2,17 @@ package tqs.backend.tqsbackend.dto;
 
 import java.math.BigDecimal;
 import java.time.LocalDate;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 import tqs.backend.tqsbackend.entity.BookingStatus;
 import tqs.backend.tqsbackend.entity.PaymentStatus;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class BookingResponse {
 
     private Long id;
@@ -16,92 +24,4 @@ public class BookingResponse {
     private BookingStatus status;
     private PaymentStatus paymentStatus;
     private String paymentReference;
-
-    public BookingResponse() {
-    }
-
-    public BookingResponse(Long id, Long itemId, Long renterId, LocalDate startDate, LocalDate endDate,
-            BigDecimal totalPrice, BookingStatus status, PaymentStatus paymentStatus, String paymentReference) {
-        this.id = id;
-        this.itemId = itemId;
-        this.renterId = renterId;
-        this.startDate = startDate;
-        this.endDate = endDate;
-        this.totalPrice = totalPrice;
-        this.status = status;
-        this.paymentStatus = paymentStatus;
-        this.paymentReference = paymentReference;
-    }
-
-    public Long getId() {
-        return id;
-    }
-
-    public void setId(Long id) {
-        this.id = id;
-    }
-
-    public Long getItemId() {
-        return itemId;
-    }
-
-    public void setItemId(Long itemId) {
-        this.itemId = itemId;
-    }
-
-    public Long getRenterId() {
-        return renterId;
-    }
-
-    public void setRenterId(Long renterId) {
-        this.renterId = renterId;
-    }
-
-    public LocalDate getStartDate() {
-        return startDate;
-    }
-
-    public void setStartDate(LocalDate startDate) {
-        this.startDate = startDate;
-    }
-
-    public LocalDate getEndDate() {
-        return endDate;
-    }
-
-    public void setEndDate(LocalDate endDate) {
-        this.endDate = endDate;
-    }
-
-    public BigDecimal getTotalPrice() {
-        return totalPrice;
-    }
-
-    public void setTotalPrice(BigDecimal totalPrice) {
-        this.totalPrice = totalPrice;
-    }
-
-    public BookingStatus getStatus() {
-        return status;
-    }
-
-    public void setStatus(BookingStatus status) {
-        this.status = status;
-    }
-
-    public PaymentStatus getPaymentStatus() {
-        return paymentStatus;
-    }
-
-    public void setPaymentStatus(PaymentStatus paymentStatus) {
-        this.paymentStatus = paymentStatus;
-    }
-
-    public String getPaymentReference() {
-        return paymentReference;
-    }
-
-    public void setPaymentReference(String paymentReference) {
-        this.paymentReference = paymentReference;
-    }
 }
